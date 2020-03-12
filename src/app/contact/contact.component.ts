7305eecec0ce56f1ee444647cd5930d90e0ed023
Eliza <elizade43000@gmail.com>
2020-03-12 18:54:05 +0100

    barre de recherche

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"]
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  public data: any = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private http: HttpClient) {}
 }
